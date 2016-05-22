b69e48c0619b67d9505567ab0ba203123a8c5fc3
zainin <thexsinner@gmail.com>
2016-05-22 23:16:04 +0200

    improve netwidgets, add underline widget

--- .config/awesome/widgets.lua
+++ .config/awesome/widgets.lua
@@ -9,6 +9,24 @@ if (string.len(tmp) >= 1) then
     isLAPTOP = false
 end
 
+--- Detect available network interfaces
+local handle = io.popen("ip -o link | awk '{sub(\":$\", \"\", $2); if ($2!=\"lo\") print $2}' | xargs")
+local tmp = handle:read("*a")
+handle:close()
+local ifaces = {}
+for i in string.gmatch(tmp, '%w+') do
+    table.insert(ifaces, i)
+end
+
+--- Get speeds from all available interfaces - helper for vicious widget
+function netspeed(direction, args)
+    local speed = 0.0
+    for _, i in pairs(ifaces) do
+        speed = speed + args["{" .. i .. " " .. direction .. "}"]
+    end
+    return speed
+end
+
 --- Pack the widget in a box
 function boxwidget(w, color)
     local background = wibox.widget.background()
@@ -20,19 +38,47 @@ function boxwidget(w, color)
     return margin
 end
 
+--- Widget with an underline
+function underline(w, ucolor)
+    local bg = wibox.widget.background()
+    bg:set_widget(w)
+    bg:set_bg(theme.bg_normal)
+    local margin = wibox.layout.margin()
+    margin:set_widget(bg)
+    margin:set_bottom(2)
+    local bg2 = wibox.widget.background()
+    bg2:set_widget(margin)
+    bg2:set_bg(ucolor)
+
+    return bg2
+end
+
 --- Systray
 -- create a systray with smaller icons
 
-systray = wibox.layout.margin()
-systray:set_widget(wibox.widget.systray())
-systray:set_margins(4)
+systray_widget = wibox.layout.margin()
+systray_widget:set_widget(wibox.widget.systray())
+systray_widget:set_margins(4)
+systray_widget = underline(systray_widget, '#d9007b')
+
+-- workaround - otherwise there's bit of an underline on the other screen
+function systray(s)
+    if s.index == 1 then
+        return systray_widget
+    end
+end
+
+--- layoutbox with underline
+function underline_box(b)
+    return underline(b, '#9999aa')
+end
 
 --- Load widget
 -- 1 minute avg sys load
 local load_content = wibox.widget.textbox()
 vicious.register(load_content, vicious.widgets.uptime,
-                "<span color='#080808'><span font='FontAwesome 9'>  </span>$4 </span>")
-loadwidget = boxwidget(load_content, '#94738c')
+                "<span color='#94738c'><span font='FontAwesome 9'>  </span>$4 </span>")
+loadwidget = underline(load_content, '#94738c')
 
 
 --- Internet widget
@@ -43,40 +89,28 @@ local ndown_content = wibox.widget.textbox()
 -- laptop and PC have different interface names, plus wifi
 vicious.register(ndown_content, vicious.widgets.net,
     function (widget, args)
-            if isLAPTOP then
-                return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
-                        .. args["{enp0s25 down_mb}"] + args["{wlp3s0 down_mb}"]
-                        .. "MB/s </span>"
-            else
-                return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
-                        .. args["{enp3s0 down_mb}"]
-                        .. "MB/s </span>"
-            end
+        return "<span color='#ce5666'><span font='FontAwesome 9'>  </span>"
+                .. netspeed('down_mb', args)
+                .. "MB/s </span>"
     end, 1)
-netdownwidget = boxwidget(ndown_content, '#ce5666')
+netdownwidget = underline(ndown_content, '#ce5666')
 
 local nup_content = wibox.widget.textbox()
 -- like with the previous one, laptop and pc
 vicious.register(nup_content, vicious.widgets.net,
     function (widget, args)
-            if isLAPTOP then
-                return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
-                        .. args["{enp0s25 up_mb}"] + args["{wlp3s0 up_mb}"]
-                        .. "MB/s </span>"
-            else
-                return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
-                        .. args["{enp3s0 up_mb}"]
-                        .. "MB/s </span>"
-            end
+        return "<span color='#87af5f'><span font='FontAwesome 9'>  </span>"
+                .. netspeed('up_mb', args)
+                .. "MB/s </span>"
     end, 1)
-netupwidget = boxwidget(nup_content, '#87af5f')
+netupwidget = underline(nup_content, '#87af5f')
 
 --- RAM widget
 
 local m_content = wibox.widget.textbox()
 vicious.register(m_content, vicious.widgets.mem,
-                "<span color='#080808'><span font='FontAwesome 9'> </span> $1% </span>")
-memwidget = boxwidget(m_content, '#7788af')
+                "<span color='#7788af'><span font='FontAwesome 9'> </span> $1% </span>")
+memwidget = underline(m_content, '#7788af')
 
 --- Battery widget
 
@@ -127,26 +161,28 @@ local fs_content = wibox.widget.textbox()
 vicious.register(fs_content, vicious.widgets.fs,
     function (widget, args)
         if isLAPTOP then
-            return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
+            return "<span color='#9c6523'><span font='FontAwesome 9'>  </span>"
                     .. args["{/ used_p}"] .. "%, "
                     .. args["{/home used_p}"] .. "% </span>"
         else
-            return "<span color='#080808'><span font='FontAwesome 9'>  </span>"
+            return "<span color='#9c6523'><span font='FontAwesome 9'>  </span>"
                     .. args["{/ used_p}"] .. "%, "
                     .. args["{/home used_p}"] .. "%, "
-                    .. args["{/media/storage-ext used_p}"] .. "% </span> "
+                    .. args["{/media/storage-ext used_p}"] .. "% </span>"
         end
     end, 10)
-fswidget = boxwidget(fs_content, '#9c6523')
+fswidget = underline(fs_content, '#9c6523')
 
 --- Clock widget
 
 local c_content = wibox.widget.textbox()
 vicious.register(c_content, vicious.widgets.date,
-                "<span color='#080808'><span font='FontAwesome 9'>  </span>%a %d/%m/%y %R </span>")
-clockwidget = boxwidget(c_content, '#9999aa')
+                "<span color='#4299d5'><span font='FontAwesome 9'>  </span>%a %d/%m/%y %R </span>")
+clockwidget = underline(c_content, '#4299d5')
 
 -- clicking the clock toggles a calander
 clockwidget:connect_signal("button::press", function()
        awful.spawn.with_shell("gsimplecal")
     end)
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
